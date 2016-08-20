3c4ba30caa5329d57ee090a84fe55efcd4aa348d
Joaquim Pessoa Filho <joaquim@MacBook-Pro-de-Joaquim-3.local>
2016-08-20 18:13:38 -0300

    Mapa parcialmente implementado

---
+++ MackTIA/CampusMap/CampusMapAnnotationView.swift
@@ -0,0 +1,21 @@
+//
+//  CampusMapAnnotationView.swift
+//  MackTIA
+//
+//  Created by Joaquim Pessoa Filho on 20/08/16.
+//  Copyright © 2016 Mackenzie. All rights reserved.
+//
+
+import UIKit
+
+class CampusMapAnnotationView: MKAnnotationView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MackTIA/CampusMap/CustomAnnotationView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomAnnotation.swift
+//  MackTIA
+//
+//  Created by Joaquim Pessoa Filho on 20/08/16.
+//  Copyright © 2016 Mackenzie. All rights reserved.
+//
+
+import MapKit
+
+class CustomAnnotation: MKAnnotationView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
